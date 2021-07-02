4380a72b07ca203828502a0702e37a7a5fd45460
Eoin Hennessy <eoin.hennessy@gmail.com>
2021-07-02 14:22:11 +0100

    Initial commit
    squash

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const webpack = require('webpack');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const dev = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  mode: dev ? 'development' : 'production',
+  devtool: 'source-map',
+  entry: {
+    app: './src/App',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: '[name].js',
+  },
+  optimization: {
+    noEmitOnErrors: true,
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: 'styles.css' }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'babel-loader' },
+          {
+            loader: '@linaria/webpack-loader',
+            options: { sourceMap: dev },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: dev },
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|png|gif|woff|woff2|eot|ttf|svg)$/,
+        use: [{ loader: 'file-loader' }],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: [path.join(__dirname, 'public')],
+    historyApiFallback: true,
+  },
+};
